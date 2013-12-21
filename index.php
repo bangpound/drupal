2c9a9e68eb2d85c18dfc8be7a2231a3eab58a7c7
Benjamin Doherty <bjd@bangpound.org>
2013-12-20 18:19:33 -0600

    initial port.

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <?php
 
-/**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * The routines here dispatch control to the appropriate handler, which then
- * prints the appropriate page.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt.
- */
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
 
-include __DIR__ . '/vendor/autoload.php';
+$loader = require_once __DIR__.'/app/bootstrap.php.cache';
 
-/**
- * Root directory of Drupal installation.
- */
-define('DRUPAL_ROOT', getcwd());
+// Use APC for autoloading to improve performance.
+// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
+// with other applications also using APC.
+/*
+$apcLoader = new ApcClassLoader('sf2', $loader);
+$loader->unregister();
+$apcLoader->register(true);
+*/
 
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL, TRUE, 'Bangpound\\Drupal\\Bootstrap\\AutoloadBootstrap');
-menu_execute_active_handler();
+require_once __DIR__.'/app/AppKernel.php';
+//require_once __DIR__.'/app/AppCache.php';
+
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
